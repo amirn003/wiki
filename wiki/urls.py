9ee9ed0555cf3e6211e184d71d3f7e947aa71a98
amirn003 <amir.naar@gmail.com>
2024-09-11 19:08:11 +0200

    start add wiki route

--- wiki/urls.py
+++ wiki/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("encyclopedia.urls"))
+    path('', include("encyclopedia.urls")),
+    path('wiki', include("encyclopedia.urls"))
 ]
