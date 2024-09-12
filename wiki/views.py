07bd45b147d2e1ae99b9ef1450ba1906431b24ed
amirn003 <amir.naar@gmail.com>
2024-09-12 09:12:22 +0200

    get the content of the encyclopedia and display it

--- wiki/views.py
+++ wiki/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from encyclopedia import util
 
 
 def search_by_title(request, name):
-    return HttpResponse(f"This is the title searched: {name}")
+    read_entry = util.get_entry(name)
+    return HttpResponse(f"<h1> {name} </h1><br> <p> {read_entry} </p>")
