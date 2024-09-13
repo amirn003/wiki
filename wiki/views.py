bee870cdb64130c52885ae404feb9bc796a4501a
amirn003 <amir.naar@gmail.com>
2024-09-13 06:04:19 +0200

    add conditional page not found if an entry is not in the encyclopedia

--- wiki/views.py
+++ wiki/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from encyclopedia import util
+from django.http import HttpResponseNotFound
 
 
 def search_by_title(request, name):
-    read_entry = util.get_entry(name)
-    return HttpResponse(f"<h1> {name} </h1><br> <p> {read_entry} </p>")
+    if util.get_entry(name):
+        read_entry = util.get_entry(name)
+        return HttpResponse(f"<h1> {name} </h1><br> <p> {read_entry} </p>")
+    else:
+        return HttpResponseNotFound("Page not found.")
