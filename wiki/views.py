48eb40853a2487674b00b25a83d358f094f8ed33
amirn003 <amir.naar@gmail.com>
2024-09-13 06:47:07 +0200

    add ResponseNotFound

--- wiki/views.py
+++ wiki/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from encyclopedia import util
+from django.http import HttpResponseNotFound
 
 
 def search_by_title(request, name):
