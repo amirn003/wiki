68b88eaaaf87003b6cb04f438332a69871e33701
amirn003 <amir.naar@gmail.com>
2024-09-13 06:45:45 +0200

    add links to the index page

--- wiki/views.py
+++ wiki/views.py
@@ -4,5 +4,8 @@ from encyclopedia import util
 
 
 def search_by_title(request, name):
-    read_entry = util.get_entry(name)
-    return HttpResponse(f"<h1> {name} </h1><br> <p> {read_entry} </p>")
+    if util.get_entry(name):
+        read_entry = util.get_entry(name)
+        return HttpResponse(f"<h1> {name} </h1><br> <p> {read_entry} </p>")
+    else:
+        return HttpResponseNotFound("Page not found.")
