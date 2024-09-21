9aa44076b32954cb93a9571cfbc08de4e5249b03
amirn003 <amir.naar@gmail.com>
2024-09-21 12:12:01 +0200

    start creating template for the article

--- wiki/views.py
+++ wiki/views.py
@@ -9,7 +9,11 @@ def index(request):
 def search_by_title(request, name):
     if util.get_entry(name):
         read_entry = util.get_entry(name)
-        return HttpResponse(f"<h1> {name} </h1><br> <p> {read_entry} </p>")
+        # return HttpResponse(f"<h1> {name} </h1><br> <p> {read_entry} </p>")
+        return render(request, "wiki/article.html")
+        # render(request, "wiki/article.html", {
+        #     "name": name
+        # })
     else:
         return HttpResponseNotFound("Page not found.")
 
