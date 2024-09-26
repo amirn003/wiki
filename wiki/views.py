660df2d50467e063ab72f6b5ccca3afa9baac9b7
amirn003 <amir.naar@gmail.com>
2024-09-26 16:58:45 +0200

    refactor search_by_query view

--- wiki/views.py
+++ wiki/views.py
@@ -18,29 +18,22 @@ def search_by_title(request, name):
 
 def search_by_query(request):
     query = request.GET.get('q')
-    if query and util.get_entry(query) and query is not None:
+    if query:
         read_entry = util.get_entry(query)
-        # return HttpResponse(f"<h1> {query} </h1><br> <p> {read_entry} </p>")
-        return render(request, "wiki/article.html", {
-            "name": query.upper(),
-            "read_entry": read_entry
-        })
-    else:
-        entries = util.list_entries()
-        matching_entries = []
-        for entry in entries:
-            if query.upper() in entry.upper():
-                matching_entries.append(entry)
+        if read_entry:
+            return render(request, "wiki/article.html", {
+                "name": query.upper(),
+                "read_entry": read_entry
+            })
 
+        matching_entries = [entry for entry in util.list_entries() if query.upper() in entry.upper()]
         if matching_entries:
-            #return HttpResponse(f"<h1> Articles found for your search: {query} </h1><br> <p> {matching_entries} </p>")
             return render(request, "wiki/matching_entries.html", {
                 "query": query,
                 "matching_entries": matching_entries
             })
 
     return render(request, "wiki/404.html")
-
 
 def edit(request, name):
     return render(request, "wiki/edit.html", {
